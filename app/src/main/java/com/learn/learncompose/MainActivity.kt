2b1f2b8d61c698166224df75bbb33dd12ad0b109
Satyam Gondhale <gondhalesatyam@gmail.com>
2022-07-03 16:33:50 +0530

    State Management in Compose

--- app/src/main/java/com/learn/learncompose/MainActivity.kt
+++ app/src/main/java/com/learn/learncompose/MainActivity.kt
@@ -6,16 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.learn.learncompose.compose.SendOtp
-import com.learn.learncompose.compose.SendOtpSuccess
 import com.learn.learncompose.ui.theme.LearnComposeTheme
 import com.learn.learncompose.viewmodel.SendOtpViewModel
 
@@ -35,9 +31,6 @@ class MainActivity : ComponentActivity() {
                             val sendOtpViewModel =
                                 ViewModelProvider(this@MainActivity).get(SendOtpViewModel::class.java)
                             SendOtp(sendOtpViewModel)
-                        }
-                        composable(route = "sendOtpSuccess") {
-                            SendOtpSuccess()
                         }
                     }
                 }
